89ab75bff21032cf524f1f5c42fe43fe1ab2a8c6
tp <tp@tpui-iMac.local>
2024-02-05 09:49:28 +0900

    #2 2월 첫째주 공부 결과 작성

--- lib/screens/basic_widget 11.dart
+++ lib/screens/basic_widget 11.dart
@@ -14,45 +14,74 @@ class _Basic_Widget_11State extends State<Basic_Widget_11> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CupertinoNavigationBar(
-        middle: Text('4.10 쿠퍼티노 디자인'),
-      ),
-      body: Column(
-        children: [
-          CupertinoSwitch(
-            value: _isOn,
-            onChanged: (bool value) {
-              setState(() {
-                _isOn = value;
-              });
-            },
-          ),
-          CupertinoButton(
-              borderRadius: BorderRadius.circular(16),
-              child: Text('쿠퍼티노 AlertDialog'),
-              onPressed: () {}),
-          CupertinoButton(
-              borderRadius: BorderRadius.circular(16),
-              child: Text('쿠퍼티노 Picker'),
-              onPressed: () {})
-        ],
-      ));
-      }
-     _showCupertinoDialog(){
-      showDialog(context: context, builder: (context) =>
-        CupertinoAlertDialog(
-          title: Text('제목'),
-          content: Text('내용'),
-          actions: [
-            CupertinoDialogAction(child: Text('Ok'),
-            onPressed: (){
-              Navigator.of(context).pop();
-            },
-            )
+        appBar: CupertinoNavigationBar(
+          middle: Text('4.10 쿠퍼티노 디자인'),
+        ),
+        body: Column(
+          children: [
+            CupertinoSwitch(
+              value: _isOn,
+              onChanged: (bool value) {
+                setState(() {
+                  _isOn = value;
+                });
+              },
+            ),
+            CupertinoButton(
+                borderRadius: BorderRadius.circular(16),
+                child: Text('쿠퍼티노 AlertDialog'),
+                onPressed: () {}),
+            CupertinoButton(
+                borderRadius: BorderRadius.circular(16),
+                child: Text('쿠퍼티노 Picker'),
+                onPressed: () {})
           ],
-        )
-      )
-      }
-    
+        ));
   }
 
+  _showCupertinoDialog() {
+    showDialog(
+        context: context,
+        builder: (context) => CupertinoAlertDialog(
+              title: Text('제목'),
+              content: Text('내용'),
+              actions: [
+                CupertinoDialogAction(
+                  child: Text('Ok'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ));
+  }
+// _showCupertinoModalPopup(
+//   {context: context,
+//   builder: (context) => Container(
+//     height: 10.0,
+//     child: CupertinoPicker(children: [
+//       // ...
+//     ]),
+//     itemExtent: 10.0,
+//     onSelectedItemChanged: (int value) {
+//       // ...
+//     },
+//   ),
+// ) {
+//   // 함수의 내용
+// }
+
+  // _showCuperTinoPicker() async {
+  //   final _items = List.generate(10, (index) => index);
+  //   var result = _items[0];
+
+  //   await showCupertinoModalPopup(context: context, builder: (context) => Container(
+  //     height: 200.0,
+  //     child: CupertinoPicker(children: [
+  //       _items.map((e) => Text(
+  //         'No. $e')).toList(),  // 0~9 숫자표시
+  //         itemExtent : 50.0, // 항목 1개 높이는 50 
+  //     ]),
+  //   ))
+  // }
+}
